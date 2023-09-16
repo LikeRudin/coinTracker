14f156ebe042e37625917be3119c02bb2ce6cf57
likeRudin <likeRudin@gmail.com>
2023-09-16 22:27:33 +0900

    set absolute path @

--- base/webpack.config.ts
+++ base/webpack.config.ts
@@ -30,6 +30,9 @@ const config: WebpackConfig = {
   devtool: isDevMode ? 'inline-source-map' : 'hidden-source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, SOURCE_DIR),
+    },
   },
   entry: `./${SOURCE_DIR}/index`,
   target: ['web', 'es5'],
